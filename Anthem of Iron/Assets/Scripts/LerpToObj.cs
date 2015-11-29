b87d54821280c72d231ef05db787b45171010e87
tylertks <tylertks@gmail.com>
2015-11-29 15:36:38 -0500

    Update Test features
    testing lerp speeds and particle collisions

---
+++ Anthem of Iron/Assets/Scripts/LerpToObj.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpToObj : MonoBehaviour {
+    public GameObject target;
+    public float moveSpeed;
+    bool go = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            go = !go;
+        }
+        if (go)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+           // Debug.Log("lerping at " + moveSpeed + " towards " + target.transform.position);
+        }
+	}
+}
