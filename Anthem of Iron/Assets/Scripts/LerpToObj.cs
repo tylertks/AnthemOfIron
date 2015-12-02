d33e8dca4c3048fb8f82826fb338089739dbe3bc
tylertks <tylertks@gmail.com>
2015-12-02 09:48:39 -0500

    AoI 0.1.1
    Test features and Weapon selection

--- Anthem of Iron/Assets/Scripts/LerpToObj.cs
+++ Anthem of Iron/Assets/Scripts/LerpToObj.cs
@@ -5,22 +5,92 @@ public class LerpToObj : MonoBehaviour {
     public GameObject target;
     public float moveSpeed;
     bool go = false;
+	public bool slerp;
+	public bool altSlerp = false;
+	private bool halfWayThere = false;
+	public float journeyTime = 1;
+	public int rect = 0;
+	Vector3 half;
+	Vector3 start;
+	Vector3 targetPos;
+	float startTime;
+	bool notStarted = false;
+	float completed;
+	float other;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		half = (transform.position + target.transform.position) * .5f;
+		start = transform.position;
+		targetPos = target.transform.position;
+		//Debug.Log (half);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetButtonDown("Jump"))
+		if(Vector3.Distance(transform.position,target.transform.position)<1)
+		{
+			go = false;
+		}
+		if(Input.GetButtonDown("Jump"))
         {
             go = !go;
+			if(notStarted)
+			{
+				startTime = Time.time;
+				//completed = journeyTime;
+			}
         }
         if (go)
         {
-            transform.position = Vector3.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
-           // Debug.Log("lerping at " + moveSpeed + " towards " + target.transform.position);
+			other+=Time.deltaTime;
+			//Debug.Log ("independent time: "+other);
+			if(slerp)
+			{
+				if(altSlerp)
+				{
+					completed += Time.deltaTime;
+					transform.position = Vector3.Slerp (start,targetPos,completed/journeyTime);
+				}
+				else
+				{
+					half -= Vector3.up;
+					Vector3 startRel = start - half;
+					Vector3 targetRel = targetPos - half;
+					completed =(Time.time - startTime)/journeyTime;
+					transform.position = Vector3.Slerp (startRel, targetRel,completed);
+					transform.position+=half;
+					//Debug.Log ("percent complete: " + completed*100 +"%");
+				}
+			}
+			else
+			{
+				if(!altSlerp)
+				{
+					transform.position = Vector3.Lerp(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+				}
+				else
+				{
+					if(!halfWayThere)
+					{
+						transform.position = Vector3.Lerp(transform.position,half, moveSpeed*2 * Time.deltaTime);
+						if(Vector3.Distance(transform.position, half)<1)
+							halfWayThere = true;
+					}
+					else
+					{
+						transform.position = Vector3.Lerp(transform.position, target.transform.position, moveSpeed*2 * Time.deltaTime);
+					}
+				}
+	           // Debug.Log("lerping at " + moveSpeed + " towards " + target.transform.position);
+			}
         }
+
+	}
+	void OnGUI()
+	{
+		Rect r = new Rect (rect * 100, 50, 100, 50);
+		GUI.Label (r, other.ToString());
 	}
 }
---
+++ Anthem of Iron/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour 
+{
+	public string explosiveType;
+	public string energyType;
+	float heatLevel;
+	float heatGauge = 100;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void EnergyFire()
+	{
+
+	}
+	void ExplosiveFire()
+	{
+
+	}
+}
+[System.Serializable]
+public class Weapon
+{
+	public string name;
+	public float range;
+	public float rateOfFire;
+	float fireTime;
+	public bool energy;
+	public bool spinUp;
+	public float spinTime;
+	float spin;
+	public bool heat;
+
+	public GameObject bulletObject;
+	public GameObject muzzleFlash;
+	public GameObject hitObject;
+
+	public GameObject shotSound;
+
+	public GameObject weaponMesh;
+
+	public void Fire(Vector3 dir)
+	{
+		if (fireTime > rateOfFire) {
+			if(energy)
+			{
+				EnergyFire(dir);
+			}
+			else
+			{
+				ExplosiveFire(dir);
+			}
+		}
+	}
+	void EnergyFire(Vector3 dir)
+	{
+
+	}
+	void ExplosiveFire(Vector3 dir)
+	{
+
+	}
+	public void Start()
+	{
+
+	}
+}
