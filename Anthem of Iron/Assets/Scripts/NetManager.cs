518087cec5ee6245986107260c49dd39c9730b7e
tylertks <tylertks@gmail.com>
2015-12-07 12:08:47 -0500

    AoI 0.1.2
    Started Custom Network Manager

---
+++ Anthem of Iron/Assets/Scripts/NetManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetManager : NetworkManager 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	void OnGUI()
+	{
+
+	}
+	void StartHost()
+	{
+		NetworkManager.singleton.StartHost ();
+	}
+}
